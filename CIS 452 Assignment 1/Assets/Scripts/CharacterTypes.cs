d9bf158f8407168eff0db914699ca3c08c907c1f
sidfoe <sidfoe2125@gmail.com>
2020-01-24 13:49:24 -0600

    the project

---
+++ CIS 452 Assignment 1/Assets/Scripts/CharacterTypes.cs
@@ -0,0 +1,15 @@
+﻿/*
+* (Sydney Foe)
+* (CharacterTypes)
+* (Assignment 1)
+* (Abstract class that holds the base attack that all characters will have)
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class CharacterTypes 
+{
+    public abstract void BaseAttack();
+}
---
+++ CIS 452 Assignment 1/Assets/Scripts/Stats.cs
@@ -0,0 +1,19 @@
+﻿/*
+* (Sydney Foe)
+* (Stats)
+* (Assignment 1)
+* (Interface that controls the getter and setter for the attack stats for both kinds of attacks.)
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Stats 
+{
+    void SetBasicAttk(int num);
+    int GetBasicAttk();
+
+    void SetTypeAttk(int num);
+    int GetTypeAttk();
+}
