670c5d7cbe9970107938916e70078fcefd3bce4d
sidfoe <sidfoe2125@gmail.com>
2020-02-20 10:15:15 -0600

    update

---
+++ CIS 452 Assignment 5/Assets/Scripts/Cylinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder : MonoBehaviour
+{
+    public Vector3 pos;
+
+    void Start()
+    {
+        //pos = transform.position;
+        //pos.y = .73f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < pos.y)
+        {
+            transform.position = pos;
+        }
+    }
+}
